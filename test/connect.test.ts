c0cd31596141b752e54e20ad17aef6017902282f
Joe Wagner <njwjs722@gmail.com>
2022-02-18 09:55:29 -0700

    Allow calling connect with an existing token

--- test/connect.test.ts
+++ test/connect.test.ts
@@ -1,5 +1,6 @@
 import fetch from "jest-fetch-mock";
 import { Signer } from "ethers";
+import flushPromises from 'flush-promises';
 import { connect } from "../src/main";
 import { ConnectionOptions } from "../src/interfaces";
 
@@ -31,6 +32,24 @@ describe("connect function", function () {
     await expect(typeof connection.list).toBe("function");
     await expect(typeof connection.query).toBe("function");
     await expect(typeof connection.create).toBe("function");
+  });
+
+  test("allows specifying connection token", async function () {
+    const connection1 = await connect({ network: "testnet", host: "https://testnet.tableland.network" });
+
+    // We wait for over 1 second so that if the two connections are generating different tokens
+    // then the expirations will be different. This will ensure the assertion will only succeed
+    //  if the passed in token value is used
+    await new Promise((resolve) => setTimeout(() => resolve(null), 1001));
+    await flushPromises();
+
+    const connection2 = await connect({
+      network: "testnet",
+      host: "https://testnet.tableland.network",
+      token: connection1.token
+    });
+
+    await expect(connection1.token.token === connection2.token.token).toBe(true);
   });
 
   test("throws error if provider network is not supported", async function () {
