7f69289fbab99c9b27f8b36cbea2fd26ea6a7cea
Joe Wagner <njwjs722@gmail.com>
2022-05-05 16:41:33 -0600

    Add Bearer token to `list` method request

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -4,7 +4,12 @@ export async function list(this: Connection): Promise<TableMetadata[]> {
   const address = await this.signer.getAddress();
 
   const resp: TableMetadata[] = await fetch(
-    `${this.host}/tables/controller/${address}`
+    `${this.host}/tables/controller/${address}`,
+    {
+      headers: {
+        Authorization: `Bearer ${this.token.token}`,
+      }
+    },
   ).then((r) => r.json());
 
   return resp;
