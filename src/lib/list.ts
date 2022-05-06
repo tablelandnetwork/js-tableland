f356da9320f97404e5ab5e68823076aed6ca41c0
Joe Wagner <njwjs722@gmail.com>
2022-05-06 12:44:26 -0600

    Remove Auth header from list, per outcome of #43

--- src/lib/list.ts
+++ src/lib/list.ts
@@ -4,12 +4,7 @@ export async function list(this: Connection): Promise<TableMetadata[]> {
   const address = await this.signer.getAddress();
 
   const resp: TableMetadata[] = await fetch(
-    `${this.host}/tables/controller/${address}`,
-    {
-      headers: {
-        Authorization: `Bearer ${this.token.token}`,
-      },
-    }
+    `${this.host}/tables/controller/${address}`
   ).then((r) => r.json());
 
   return resp;
