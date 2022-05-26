1b72e9e5289fe4440b0bddf013888b66d1996765
Joe Wagner <njwjs722@gmail.com>
2022-05-26 14:14:31 -0600

    Update local contract address

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -14,7 +14,7 @@ const polyfills = {
 export const btoa = globalThis.btoa ?? polyfills.btoa;
 
 export const contractAddresses: { [index: string]: string } = {
-  local: "0x5fbdb2315678afecb367f032d93f642f64180aa3", // If building locally you can put your contract address here
+  local: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512", // If building locally you can put your contract address here
   "optimism-kovan-staging": "0xa01C278F1Ea79D11D008b1b7DB96258dAD7621E4",
   staging: "0x847645b7dAA32eFda757d3c10f1c82BFbB7b41D0",
   testnet: "0x30867AD98A520287CCc28Cde70fCF63E3Cdb9c3C",
