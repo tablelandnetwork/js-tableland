ed6c06e7727e61a11196c2e7eb95ffced3e6077c
Joe Wagner <njwjs722@gmail.com>
2022-06-17 18:01:14 -0600

    fix: list method returns empty array if no tables

--- test/list.test.ts
+++ test/list.test.ts
@@ -1,9 +1,9 @@
 import fetch from "jest-fetch-mock";
-import { FetchMyTables } from "../test/fauxFetch";
+import { FetchMyTables, FetchNoTables } from "../test/fauxFetch";
 import { connect } from "../src/main";
 
 describe("list method", function () {
-  let list: any, connection: any;
+  let connection: any;
   beforeAll(async function () {
     // reset in case another test file hasn't cleaned up
     fetch.resetMocks();
@@ -11,7 +11,6 @@ describe("list method", function () {
       network: "testnet",
       host: "https://testnet.tableland.network",
     });
-    list = connection.list;
   });
 
   afterEach(function () {
@@ -22,7 +21,7 @@ describe("list method", function () {
   test("When I fetch my tables, I get some tables", async function () {
     fetch.mockResponseOnce(FetchMyTables);
 
-    const resp = await list.call(connection);
+    const resp = await connection.list();
     const table = resp[0];
 
     await expect(table.name).toEqual("test_list_query_table");
@@ -31,5 +30,13 @@ describe("list method", function () {
       "7837fa79ed5151d99da5051b41d7387e7c249a2b0321d440138c81108160cdd9"
     );
     await expect(table.created_at).toEqual("2022-02-11T02:12:19.80809Z");
+  });
+
+  test("If I have no tables, I get empty Array", async function () {
+    fetch.mockResponseOnce(FetchNoTables);
+
+    const resp = await connection.list();
+
+    await expect(resp).toEqual([]);
   });
 });
