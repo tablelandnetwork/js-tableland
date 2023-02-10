314564fa1491996c3cec48681cdddd9dd3978529
Joe Wagner <njwjs722@gmail.com>
2023-02-10 12:21:41 -0700

    Add browser tests for Validator and Registry

--- test/browser/server/src/App.vue
+++ test/browser/server/src/App.vue
@@ -1,6 +1,11 @@
 <script lang="ts">
 
-import { Database, helpers } from "../../../../dist/esm/index.js";
+import {
+  Database,
+  Validator,
+  Registry,
+  helpers
+} from "../../../../dist/esm/index.js";
 import { getDefaultProvider, Wallet } from "ethers";
 
 const wallet = new Wallet(
@@ -13,10 +18,14 @@ const db = new Database({
   autoWait: true,
 });
 
+const validator = new Validator(db.config);
+const registry = new Registry(db.config);
+
 export default {
   data: function () {
     return {
       tablename: "",
+      tableId: "",
       message: "",
       statement: "",
       successMsg: "",
@@ -44,6 +53,23 @@ export default {
       } catch (err) {
         this.$data.message = `runSql Error: ${err}`;
       }
+    },
+    validatorHealth: async function () {
+      const isHealthy = await validator.health();
+
+      this.$data.message = `validator health is ${isHealthy ? "good" : "not good"}`;
+    },
+    getTable: async function () {
+      const { name, schema } = await validator.getTableById({ chainId: 31337, tableId: this.$data.tableId });
+
+      this.$data.message = `table name: ${name}`
+      this.$data.responseJson = JSON.stringify(schema);
+    },
+    getMyTables: async function () {
+      const tables = await registry.listTables();
+
+      this.$data.message = "got my tables";
+      this.$data.responseJson = JSON.stringify(tables);
     }
   }
 }
@@ -61,9 +87,22 @@ export default {
 
   <main>
     <form @submit.prevent="eve => runSql()">
-      <input type="text" name="statement" v-model="statement">
-      <input type="text" name="success" v-model="successMsg">
-      <input type="submit" name="submit" value="submit">
+      <input type="text" name="statement" v-model="statement" placeholder="sql statement">
+      <input type="text" name="success" v-model="successMsg" placeholder="success message">
+      <input type="submit" name="submit-sql" value="submit">
+    </form>
+
+    <form @submit.prevent="eve => validatorHealth()">
+      <input type="submit" name="health" value="submit">
+    </form>
+
+    <form @submit.prevent="eve => getTable()">
+      <input type="text" name="tableid" v-model="tableId" placeholder="table id">
+      <input type="submit" name="get-table" value="submit">
+    </form>
+
+    <form @submit.prevent="eve => getMyTables()">
+      <input type="submit" name="get-my-tables" value="submit">
     </form>
   </main>
 </template>
